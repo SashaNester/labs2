2b23fa42ee5a9a0b6f374c184acac32f92870350
Александра Нестерова <nesterv04@mail.ru>
2025-12-03 02:14:06 +0300

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -437,4 +437,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Александра Нестерова'
-    return render_template('example.html', name=name)
+    number = 2
+    group = 'ФБИ-32'
+    course = 3
+    return render_template('example.html', name=name, number=number, group=group, course=course)

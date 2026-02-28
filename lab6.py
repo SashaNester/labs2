d675ef14300aa11e7536c017b464564877c5a244
Александра Нестерова <nesterv04@mail.ru>
2026-02-28 21:13:40 +0300

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -54,6 +54,38 @@ def api():
                     'id': id
                 }
 
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+
+        for office in offices:
+            if office['number'] == office_number:
+
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Not your office'
+                        },
+                        'id': id
+                    }
+
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
     return {
         'jsonrpc': '2.0',
         'error': {
@@ -62,3 +94,5 @@ def api():
         },
         'id': id
     }
+
+
